014a4cbc57a11f2cdcf28f93947ad7340914111b
mkpbd <115967667+kamalpassa@users.noreply.github.com>
2025-09-30 10:49:26 +0600

    update

---
+++ basic_02/day-2/loop-2.py
@@ -0,0 +1,17 @@
+##  Multiplication 
+
+def multiply(mul):
+    for num in range(1,11):
+        print( mul ,'x',num,' = ',mul*(num)) 
+
+multiply(20)
+
+rr = 11
+i = 0;
+while i < rr:
+    print(i)
+    i = i+2 
+
+
+
+
