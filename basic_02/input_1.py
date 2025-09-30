5394d511922c482f4b2c0c2907ed966e5a4b3f2c
mkpbd <mostofakamal1990@gmail.com>
2025-09-30 23:55:58 +0600

    list  assign accessing  traversing

---
+++ basic_02/input_1.py
@@ -0,0 +1,9 @@
+user_name = input("your Name :"); # input function  every time takes  string formate values
+
+
+print('Hello ',user_name); 
+
+number_input = int(input("input value integer: "))
+
+bool_input = bool(input('input True or false : '))
+
