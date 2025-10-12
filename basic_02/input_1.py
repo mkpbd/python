6b90ad9f64ab0a386bc765ca562b4dc474b54c3c
mkpbd <mostofakamal1990@gmail.com>
2025-10-13 05:54:07 +0600

    file handling

--- basic_02/input_1.py
+++
@@ -1,9 +0,0 @@
-user_name = input("your Name :"); # input function  every time takes  string formate values
-
-
-print('Hello ',user_name); 
-
-number_input = int(input("input value integer: "))
-
-bool_input = bool(input('input True or false : '))
-
